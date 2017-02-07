04ce68b60a0d7c8438c85f475c1d514e0889080b
hideyuki arakawa <hideyuki-kreva@hotmail.co.jp>
2017-02-07 20:54:28 +0900

    Lesson6課題提出
    3Dゲームアプリを作ろう Vol.2

---
+++ Assets/ItemD.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemD : MonoBehaviour {
+    private GameObject unitychan;
+	// Use this for initialization
+	void Start () {
+        unitychan = GameObject.Find("unitychan");
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (gameObject.transform.position.z < unitychan.transform.position.z - 5)
+        {
+            Destroy(gameObject);
+        }
+	}
+}
